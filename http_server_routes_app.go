07db99da5b1f2e06f840ff5ed49952514d4c7058
Robin <lxs5171@mavs.uta.edu>
2021-06-29 11:51:10 +0800

    version: 1.17.111.43.38.16.25-1

--- http_server_routes_app.go
+++ http_server_routes_app.go
@@ -166,7 +166,7 @@ func (j *JadeSDK) createHTTPHandler(moduleName string, moduleInst interface{}, m
 				var reportTo []*Interface
 				// report to upper tier aggregators
 				if moduleType == AppModuleAggregator {
-					// TODO: task could be nil
+					// TODO: task could be nil only in aggregator
 					reportTo = j.AggregativeTaskCache.GetReportTo(task.TaskID)
 				}
 				if len(req.To) > 0 {
