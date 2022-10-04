e4b79eb6d666390bf7cb5fc4b55641e689d1e302
Robin <lxs5171@mavs.uta.edu>
2022-10-04 14:48:58 -0500

    3-dev-1.0.11

--- message.go
+++ message.go
@@ -52,7 +52,7 @@ func (j *JadeSDK) sendMessages(task *SDKTask, from *Interface, to []*Interface, 
 	var requestLength int
 	for _, i := range to {
 		if !i.IsValid() {
-			j.log.Printf("the interface is not valid:", i)
+			j.log.Printf("the interface is not valid, module: ", i.ModuleName, ", node: ", i.Node)
 			continue
 		}
 		_, reqlen, _, err := j.HTTPCommunicate(
